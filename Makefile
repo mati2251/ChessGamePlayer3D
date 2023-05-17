c236e4727a159bd74a8410beae512af1e8440877
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-05-17 21:57:05 +0200

    [removed] removed unused files

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 LIBS=-lGL -lglfw -lGLEW
-HEADERS=constants.h  lodepng.h shaderprogram.h
-FILES=lodepng.cpp  main_file.cpp  shaderprogram.cpp
+HEADERS=
+FILES=main_file.cpp
 
 main_file: $(FILES) $(HEADERS)
 	g++ -o main_file $(FILES)  $(LIBS) -I.
--- main_file.cpp
+++ main_file.cpp
@@ -1,22 +1,3 @@
-/*
-Niniejszy program jest wolnym oprogramowaniem; możesz go
-rozprowadzać dalej i / lub modyfikować na warunkach Powszechnej
-Licencji Publicznej GNU, wydanej przez Fundację Wolnego
-Oprogramowania - według wersji 2 tej Licencji lub(według twojego
-wyboru) którejś z późniejszych wersji.
-
-Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on
-użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej
-gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH
-ZASTOSOWAŃ.W celu uzyskania bliższych informacji sięgnij do
-Powszechnej Licencji Publicznej GNU.
-
-Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
-Powszechnej Licencji Publicznej GNU(GNU General Public License);
-jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
-Place, Fifth Floor, Boston, MA  02110 - 1301  USA
-*/
-
 #define GLM_FORCE_RADIANS
 
 #include <GL/glew.h>
@@ -26,21 +7,15 @@ Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 #include <glm/gtc/matrix_transform.hpp>
 #include <stdlib.h>
 #include <stdio.h>
-#include "constants.h"
-#include "allmodels.h"
-#include "lodepng.h"
-#include "shaderprogram.h"
 
 void error_callback(int error, const char *description) {
     fputs(description, stderr);
 }
 
 void initOpenGLProgram(GLFWwindow *window) {
-    initShaders();
 }
 
 void freeOpenGLProgram(GLFWwindow *window) {
-    freeShaders();
 }
 
 void drawScene(GLFWwindow *window) {
@@ -53,15 +28,15 @@ int main(void) {
     glfwSetErrorCallback(error_callback);
 
     if (!glfwInit()) {
-        fprintf(stderr, "Nie można zainicjować GLFW.\n");
+        fprintf(stderr, "Can't initialize GLFW.\n");
         exit(EXIT_FAILURE);
     }
 
     window = glfwCreateWindow(500, 500, "OpenGL", NULL,
-                              NULL);  //Utwórz okno 500x500 o tytule "OpenGL" i kontekst OpenGL.
+                              NULL);
 
     if (!window) {
-        fprintf(stderr, "Nie można utworzyć okna.\n");
+        fprintf(stderr, "Can't create window.\n");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
@@ -70,7 +45,7 @@ int main(void) {
     glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Nie można zainicjować GLEW.\n");
+        fprintf(stderr, "Can't initialize GLEW.\n");
         exit(EXIT_FAILURE);
     }
 
