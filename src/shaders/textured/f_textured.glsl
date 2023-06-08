ffa48a8563833ef2bf72f5711ed4cd84d83fbb64
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-06-08 11:02:38 +0200

    [added] added lambert light

--- src/shaders/textured/f_textured.glsl
+++ src/shaders/textured/f_textured.glsl
@@ -1,12 +1,25 @@
 #version 330
 
-
-uniform sampler2D tex;
+uniform sampler2D textureMap0;
 
 out vec4 pixelColor;
 
-in vec2 i_tc;
+in vec2 iTexCoord0;
+in vec4 ic;
+in vec4 n;
+in vec4 l;
+in vec4 v;
 
 void main(void) {
-    pixelColor=texture(tex,i_tc);
+
+    vec4 ml = normalize(l);
+    vec4 mn = normalize(n);
+    vec4 mv = normalize(v);
+    vec4 mr = reflect(-ml, mn);
+
+    vec4 kd = texture(textureMap0, iTexCoord0);
+    vec4 ks = vec4(0.5, 0.5, 0.5, 0.5);
+    float nl = clamp(dot(mn, ml), 0, 1);
+    float rv = pow(clamp(dot(mr, mv), 0, 1), 100);
+    pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
 }
--- src/shaders/textured/v_textured.glsl
+++ src/shaders/textured/v_textured.glsl
@@ -6,13 +6,26 @@ uniform mat4 M;
 
 
 
-layout (location=0) in vec4 vertex;
+layout (location=0) in vec3 vertex;
+layout (location=1) in vec3 normal;
 layout (location=2) in vec2 texCoord;
 
+out vec4 l;
+out vec4 n;
+out vec4 v;
 
-out vec2 i_tc;
+
+
+out vec2 iTexCoord0;
+out vec4 pos;
 
 void main(void) {
-    gl_Position=P*V*M*vertex;
-    i_tc=texCoord;
+    vec4 ver = vec4(vertex, 1.0);
+    gl_Position=P*V*M*ver;
+    vec4 lp = vec4(2, 2, -6, 1);
+    l = normalize(V * lp - V*M*ver);
+    v = normalize(vec4(0, 0, 0, 1) - V * M * ver);
+    n = normalize(V * M * vec4(normal,0));
+    pos = ver;
+    iTexCoord0=texCoord;
 }
